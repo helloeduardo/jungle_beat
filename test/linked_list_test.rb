92fdfd37ffe7d0426c35723b5ae0901c4bb0602d
Eduardo Parra <helloeduardo@icloud.com>
2020-11-25 09:59:34 -0700

    Add tests for multiple nodes

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -35,4 +35,52 @@ class NodeTest < Minitest::Test
 
     assert_equal 1, list.count
   end
+
+  def test_it_can_return_string
+    list = LinkedList.new
+
+    list.append('doop')
+
+    assert_equal 'doop', list.to_string
+  end
+
+  def test_it_can_append_multiple
+    list = LinkedList.new
+
+    list.append('doop')
+    list.append('deep')
+
+    assert_equal 'doop', list.head.data
+    assert_equal 'deep', list.head.next_node.data
+  end
+
+  def test_it_can_count_multiple
+    list = LinkedList.new
+
+    assert_equal 0, list.count
+
+    list.append('doop')
+
+    assert_equal 1, list.count
+
+    list.append('deep')
+
+    assert_equal 2, list.count
+
+    list.append('beep')
+
+    assert_equal 3, list.count
+  end
+
+  def test_it_can_return_string_for_multiple
+    list = LinkedList.new
+
+    list.append('doop')
+
+    assert_equal 'doop', list.to_string
+
+    list.append('deep')
+
+    assert_equal 'doop deep', list.to_string
+  end
 end
