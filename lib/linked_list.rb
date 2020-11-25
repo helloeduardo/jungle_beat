19892db1ee1692da8c9301b090d510d39a7e50ff
Eduardo Parra <helloeduardo@icloud.com>
2020-11-24 18:56:26 -0700

    Add append and count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,20 @@
 class LinkedList
-  
+  attr_reader :head
+
+  def initialize
+    @head = nil
+  end
+
+  def append(data)
+    @head = Node.new(data)
+  end
+
+  def count
+    return 0 if head.nil?
+    counter = 1
+    while head.next_node
+      counter += 1
+    end
+    counter
+  end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -9,4 +9,30 @@ class NodeTest < Minitest::Test
 
     assert_instance_of LinkedList, list
   end
+
+  def test_it_has_a_head
+    list = LinkedList.new
+
+    assert_nil list.head
+  end
+
+  def test_it_can_append
+    list = LinkedList.new
+
+    list.append('doop')
+
+    assert_instance_of Node, list.head
+    assert_equal 'doop', list.head.data
+    assert_nil list.head.next_node
+  end
+
+  def test_it_can_count
+    list = LinkedList.new
+
+    assert_equal 0, list.count
+
+    list.append('doop')
+
+    assert_equal 1, list.count
+  end
 end
